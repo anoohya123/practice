3c7d4967046c8dd383d709eefd37f4c4fd34b24c
ppow-anoohyaa <anoohyaa@purchasingpower.com>
2021-03-03 20:38:46 +0530

    Added Hello world Project

---
+++ helloworld/src/main/java/com/springboot/starter/helloController.java
@@ -0,0 +1,27 @@
+package com.springboot.starter;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class helloController {
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String hello(Model model){
+        model.addAttribute("message","HELLO WORLD !");
+        return "hello";
+    }
+
+    @RequestMapping(value="/style", method = RequestMethod.GET)
+    public String applyStyle(){
+        return "applyStyle";
+    }
+
+    @RequestMapping(value="/bootstrap", method = RequestMethod.GET)
+    public String applyBootstrap(){
+        return "applyBootstrap";
+    }
+}
